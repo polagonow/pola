54483ff5a53019bef904d8f67be77ede4630d05f
paulgrammer <paulmugaya@live.com>
2026-04-01 07:31:34 +0200

    refactor: clean up

--- test/e2e/public/assets/global-error-ZGOAS6QO.js
+++
@@ -1,1 +0,0 @@
-"use client";import{a as t}from"./chunks/chunk-BYO7LABW.js";import{b as o}from"./chunks/chunk-S6KIM4VV.js";var r=o(t());function n({error:i,reset:e}){return(0,r.jsx)("html",{children:(0,r.jsxs)("body",{children:[(0,r.jsx)("h2",{children:"Something went wrong!"}),(0,r.jsx)("button",{onClick:()=>e(),children:"Try again"})]})})}export{n as default};
